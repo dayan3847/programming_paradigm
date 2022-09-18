ce5621622e2db24606cf1b9ebb7d0eff88c3f2d2
Dayan Bravo <dayan3847@gmail.com>
2022-09-18 03:48:20 -0400

    task5 palindrome recursive

---
+++ task5/palindrome_recursive.py
@@ -0,0 +1,18 @@
+# Task5: Palindrome Recursive
+
+def palindrome_recursive(word):
+    if 1 >= len(word):
+        return True
+    if word[0] != word[-1]:
+        return False
+    return palindrome_recursive(word[1:-1])
+
+
+if __name__ == '__main__':
+    print('Task5: Palindrome Recursive')
+    my_word = input('Enter a Word: ')
+    is_palindrome = palindrome_recursive(my_word)
+    if is_palindrome:
+        print('\033[92m' + f'The word "{my_word}" is a palindrome')
+    else:
+        print('\033[91m' + f'The word "{my_word}" is not a palindrome')
